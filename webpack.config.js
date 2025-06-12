d334faad3ab6d6c7fed6337295a93945cc2e4542
flosch62 <schwarz.flori.88@gmail.com>
2025-06-12 09:37:14 +0200

    Update .vscodeignore to include node_modules and modify package.json to add webpack-cli and bundledDependencies

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ const config = {
   devtool: 'source-map',
   externals: {
     vscode: 'commonjs vscode',
+    'openapi-typescript': 'commonjs openapi-typescript',
   },
   ignoreWarnings: [{
     module: /node_modules\/handlebars/
